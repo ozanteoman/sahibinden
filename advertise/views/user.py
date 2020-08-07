c975d286f151f4b43212528b4994c595f5f30b9c
ozanteoman <ozanteomandayanan@gmail.com>
2020-08-07 15:29:28 +0300

    update profile info

--- advertise/views/user.py
+++ advertise/views/user.py
@@ -7,8 +7,17 @@ from advertise.forms import UpdateUserForm
 from advertise.models import User
 
 
+@require_http_methods(["GET", "POST"])
 def edit_user(request, username):
     user = get_object_or_404(User, username=username)
+
+    if request.method == "POST":
+        form = UpdateUserForm(instance=user, data=request.POST or None)
+        if form.is_valid():
+            user = form.save(commit=True)
+            user_retrieve = render_to_string('user/includes/retrieve.html', context=user.info, request=request)
+            return JsonResponse(data={'user_retrieve': user_retrieve})
+
     form = UpdateUserForm(instance=user)
     string_form = render_to_string('user/includes/edit-form.html', context={'form': form}, request=request)
     return JsonResponse(data={'string_form': string_form})
